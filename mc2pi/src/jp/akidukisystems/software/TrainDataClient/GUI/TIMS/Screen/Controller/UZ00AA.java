6c0e57b68641ad018ff08af53365602253e9f011
akidukisystems <admin@akidukisystems.com>
2025-11-25 22:29:03 +0900

    特殊機能画面を追加

--- mc2pi/src/jp/akidukisystems/software/TrainDataClient/GUI/TIMS/Screen/Controller/UZ00AA.java
+++ mc2pi/src/jp/akidukisystems/software/TrainDataClient/GUI/TIMS/Screen/Controller/UZ00AA.java
@@ -1,5 +1,7 @@
 package jp.akidukisystems.software.TrainDataClient.GUI.TIMS.Screen.Controller;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,21 +9,36 @@ import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.transform.Scale;
+import javafx.util.Duration;
 import jp.akidukisystems.software.TrainDataClient.TDCCore;
 import jp.akidukisystems.software.TrainDataClient.GUI.TIMS.BaseController;
 
-public class UA00AA extends BaseController 
+public class UZ00AA extends BaseController 
 {
     @FXML private Button btnS00AB;
-    @FXML private Button btnUA01AA;
-    @FXML private Button btnUZ00AA;
+    @FXML private Button btnUA00AA;
     @FXML private Label title;
+    @FXML private Label speed;
+    @FXML private Label move;
+
     Timeline timeline;
 
     @Override
     public void init(TDCCore core)
     {
         super.init(core);
+
+        timeline = new Timeline
+        (
+            new KeyFrame
+            (
+                Duration.millis(200),   // 更新間隔（ms）
+                e -> update()
+            )
+        );
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+        update();
     }
 
     @FXML
@@ -29,9 +46,17 @@ public class UA00AA extends BaseController
     {
         btnS00AB.setOnAction(e -> goNext("/jp/akidukisystems/software/TrainDataClient/GUI/TIMS/Screen/View/S00AB.fxml"));
         btnS00AB.setText("初期\n選択");
-        btnUA01AA.setOnAction(e -> goNext("/jp/akidukisystems/software/TrainDataClient/GUI/TIMS/Screen/View/UA01AA.fxml"));
-        btnUZ00AA.setOnAction(e -> goNext("/jp/akidukisystems/software/TrainDataClient/GUI/TIMS/Screen/View/UZ00AA.fxml"));
+        btnUA00AA.setOnAction(e -> goNext("/jp/akidukisystems/software/TrainDataClient/GUI/TIMS/Screen/View/UA00AA.fxml"));
+        btnUA00AA.setText("設定\nメニュー");
         title.getTransforms().add(new Scale(2, 1, 0, 0));
+    }
+
+    private void update()
+    {
+        if (core != null && tc != null)
+        {
+
+        }
     }
 
     @Override
